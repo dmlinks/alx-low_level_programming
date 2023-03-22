61eaa5956ba4d71b65cdf3ce1685e0320eea355a
dmlinks <dosunmoyosore@gmail.com>
2023-03-22 12:06:57 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,28 +9,30 @@ void times_table(void)
 	int b;
 	int c;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			if (b == 0)
+			if ((c / 10) == 0)
 			{
+				if (b != 0)
+					_putchar(' ');
 				_putchar(c + '0');
-			}
-			else if (c < 10 && b != 0)
-			{
+
+				if (b == 9)
+					continue;
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(c + '0');
 			}
 			else
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
+				if (b == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
