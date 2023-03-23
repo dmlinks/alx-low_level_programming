47908d9f0f63b5a4bd9d91be8ab6a1ae3af73455
dmlinks <dosunmoyosore@gmail.com>
2023-03-23 01:12:00 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,14 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a => b && b => c)
 		largest = a;
 
-	else if (b > a && b > c)
+	else if (b => a && b => c)
 		largest = b;
 
-	else if (c > a && c > b)
+	else if (c => a && c => b)
 		largest = c;
-	else
-		break;
-	return (0);
+
+	return (largest);
 }
