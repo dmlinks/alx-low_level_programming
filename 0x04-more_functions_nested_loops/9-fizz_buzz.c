dce5c8326ee8d675ef6e8123cbb2e1b0d2976b00
dmlinks <dosunmoyosore@gmail.com>
2023-03-23 14:08:26 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,14 @@ int main(void)
 	{
 		if (a % 3 == 0 && a % 5 == 0)
 			printf("FizzBuzz ");
-		else if (a % 3 == 0)
+		else if (a % 3 == 0 && a % 5 != 0)
 			printf("Fizz ");
-		else if (a % 5 == 0)
+		else if (a % 5 == 0 && a % 3 != 0)
 			printf("Buzz ");
 		else
 			printf("%d ", a);
 	}
-	printf("Buzz\n");
+	printf("\n");
 
 	return (0);
 }
