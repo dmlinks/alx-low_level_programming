eb56bbb4b68c39a7ab1b194a2bedbff3015c2d09
dmlinks <dosunmoyosore@gmail.com>
2023-04-03 11:14:18 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
 
 /**
  * _strchr - Returns a pointer to the first occurrence of the char c
- * in a string s, or NULL if chat not found
+ * in a string s, or NULL if char not found
  * @s: string to search in
  * @c: char to find in s
  * Return: pointer to first occurrence or null
@@ -13,7 +13,7 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-			return (s);
+			return ((char *)s);
 		s++;
 	}
 	return (NULL);
