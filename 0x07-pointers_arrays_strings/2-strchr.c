2c5a07685e73e5a8172792cc0fc7f89169f97c2d
dmlinks <dosunmoyosore@gmail.com>
2023-04-03 11:10:28 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - Returns a pointer to the first occurrence of the char c
+ * in a string s, or NULL if chat not found
+ * @s: string to search in
+ * @c: char to find in s
+ * Return: pointer to first occurrence or null
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
