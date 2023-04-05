a074fc3ccf68328febadea513fe1b775eb5001c4
dmlinks <dosunmoyosore@gmail.com>
2023-04-05 13:20:21 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,10 +9,12 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y == 1 || y == 0)
+	if (y == 1)
 		return (x);
 	else if (y < 0)
 		return (-1);
+	else if (y == 0)
+		return (1);
 	else
 		return (x * _pow_recursion(x, y - 1));
 }
