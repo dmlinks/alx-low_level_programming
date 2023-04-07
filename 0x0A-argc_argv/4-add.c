5e58dbb5e9f1f5a577953269b400e1194511051a
dmlinks <dosunmoyosore@gmail.com>
2023-04-07 14:08:45 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - A program that multiplies two numbers
+ * @argc: The arguments' counter
+ * @argv: The arguments' values
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", sum);
+		return (0);
+	}
+
+	for (i = 0; i < argc, i++)
+	{
+		char *arg = argv[i];
+
+		for (int j = 0; arg[j]; j++)
+		{
+			if (!isdigit(arg[j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(arg);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
