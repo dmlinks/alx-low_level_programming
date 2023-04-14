d01b22b9207844f6783fc89e87ee87f6eaa227b6
dmlinks <dosunmoyosore@gmail.com>
2023-04-14 04:07:45 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,26 +10,34 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
+	char *new_location, *new_ptr;
+	unsigned int i = 0;
 
-	if (new_size == 0)
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL) /* treat as normal malloc */
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL)
-		return (malloc(new_size));
 
-	if (new_size <= old_size)
-		return (ptr);
+	new_location = malloc(new_size);
+	new_ptr = ptr;
 
-	new_ptr = malloc(new_size);
+	if (old_size > new_size)
+		old_size = new_size;
 
-	if (new_ptr == NULL)
-		return (NULL);
+	for (i = 0; i < old_size; i++)
+		new_location[i] = new_ptr[i];
 
-	memcpy(new_str, ptr, old_size);
 	free(ptr);
-
-	return (new_ptr);
+	return (new_location);
 }
