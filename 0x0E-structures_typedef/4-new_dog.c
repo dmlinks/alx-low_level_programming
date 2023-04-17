f4b7c44fda8e5b8e752b5cf084d07b9db3d27c9c
dmlinks <dosunmoyosore@gmail.com>
2023-04-17 13:26:12 -0700

    free dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,46 +2,48 @@
 #include <stdlib.h>
 
 /**
- * new_dog - creates a new dog structure
- * @name: Dog's name
- * @age: Dog's age
- * @owner: Dog's owner
- * Return: returns a pointer to newly created dog structure
+ * new_dog - create a new dog structure
+ * @name: pointer to a string containing the name of the dog
+ * @age: the age of the dog
+ * @owner: pointer to a string containing the name of the dog's owner
+ *
+ * Return: pointer to the new dog struct, or NULL if memory allocation fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *cpyname, *cpyowner;
-	int len_name = 0, len_owner = 0, i;
-
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
-	while (name[len_name])
-		len_name++;
-	while (owner[len_owner])
-		len_owner++;
+	int name_len, owner_len;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	cpyname = malloc(len_name + 1);
-	if (cpyname == NULL)
+	name_len = 0;
+	while (name[name_len] != '\0')
+		name_len++;
+	new_dog->name = malloc(name_len + 1);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
 		return (NULL);
-	for (i = 0; name[i]; i++)
-		cpyname[i] = name[i];
-	cpyname[i] = '\0';
+	}
+	for (int i = 0; i <= name_len; i++)
+		new_dog->name[i] = name[i];
 
-	cpyowner = malloc(len_owner + 1);
-	if (cpyowner == NULL)
+	owner_len = 0;
+	while (owner[owner_len] != '\0')
+		owner_len++;
+	new_dog->owner = malloc(owner_len + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
-	for (i = 0; owner[i]; i++)
-		cpyowner[i] = owner[i];
-	cpyowner[i] = '\0';
+	}
+	for (int i = 0; i <= owner_len; i++)
+		new_dog->owner[i] = owner[i];
 
-	new_dog->name = cpyname;
 	new_dog->age = age;
-	new_dog->owner = cpyowner;
+
 	return (new_dog);
 }
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees a dog structure
+ * @d: pointer to struct to free
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+
+	return;
+}
