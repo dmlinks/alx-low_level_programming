36a42825aec08eb69cf83e6a1b6c1abe89f68f10
dmlinks <dosunmoyosore@gmail.com>
2023-04-20 15:58:37 -0700

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,8 +27,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (separator != NULL || i != (n - 1))
 			printf("%s", separator);
-		else
-			continue;
 	}
 	printf("\n");
 	va_end(ap);
