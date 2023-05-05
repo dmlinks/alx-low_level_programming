776d56e268f58f2aa099b4fa0507ab9435dc81b1
dmlinks <dosunmoyosore@gmail.com>
2023-05-05 14:13:43 -0700

    Task 5

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,12 +9,12 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask;
+	unsigned long int mask = 0x01;
 
-	if (index >= sizeof(unsigned long int) * 8)
+	mask = ~(mask << index);
+	if (mask == 0x00)
 		return (-1);
 
-	mask = 1UL << index;
-	*n &= -mask;
+	*n &= mask;
 	return (1);
 }
