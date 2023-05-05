eaef432f9e1f0bef382e148445438ce608e50ef0
dmlinks <dosunmoyosore@gmail.com>
2023-05-05 13:36:10 -0700

    Updated code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,23 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int base = 1, result = 0;
-	int i;
+	unsigned int result = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
+	for (; *b != '\0'; b++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (*b != '0' && *b != '1')
 			return (0);
-	}
-
-	for (i = i - 1; i >= 0; i--)
-	{
-		if (b[i] == '1')
-			result += base;
-		base *= 2;
+		result <<= 1;
+		if (*b == '1')
+			result += 1;
 	}
 
 	return (result);
