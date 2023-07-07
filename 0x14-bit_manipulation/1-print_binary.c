4d79c86dbddce69a98cfb8286397cf9da200c2d3
dmlinks <dosunmoyosore@gmail.com>
2023-07-07 05:05:27 -0700

    binary print

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <limits.h>
+
 
 /**
  * print_binary - A function that prints the binary representation of a number
@@ -10,15 +10,18 @@
 void print_binary(unsigned long int n)
 {
 	if (n == 0)
+	{
 		_putchar('0');
+		return;
+	}
 
 	else if (n == 1)
-		_putchar('1');
-
-	else
 	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
+		_putchar('1');
+		return;
 	}
+
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+
 }
